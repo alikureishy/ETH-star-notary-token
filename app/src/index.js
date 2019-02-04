de16f36e86f40a0c3644be9f6b719d6280877289
Safdar Kureishy <safdar.kureishy@gmail.com>
2019-02-03 22:40:46 -0800

    Fixed index.js and index.html

--- app/src/index.js
+++ app/src/index.js
@@ -31,17 +31,50 @@ const App = {
     status.innerHTML = message;
   },
 
-  createStar: async function() {
+  setQueryResult: function(message) {
+    const queryResult = document.getElementById("queryResult");
+    queryResult.innerHTML = message;
+  },
+
+  createStar: async function () {
+    let self = this;
     const { createStar } = this.meta.methods;
     const name = document.getElementById("starName").value;
+    const dec = document.getElementById("starDec").value;
+    const mag = document.getElementById("starMag").value;
+    const ent = document.getElementById("starEnt").value;
+    const story = document.getElementById("starStory").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
-    App.setStatus("New Star Owner is " + this.account + ".");
+    await createStar(name, dec, mag, ent, story, id).send({ from: self.account }, function (error, result) {
+      if (!error) {
+        // console.log("Result: ", result, typeof (result));
+        App.setStatus("Account: " + self.account + " has just purchased star with token id: " + id);
+      } else {
+        alert(error)
+      }
+    });
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
+  lookUp: async function () {
+    let self = this;
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const starId = document.getElementById("queryId").value;
+    await lookUptokenIdToStarInfo(starId).call({ from: self.account }, null, function(error, result) {
+      if (!error) {
+        // console.log("Result: ", result, typeof (result));
+        const name = result[0];
+        const dec = result[1];
+        const mag = result[2];
+        const ent = result[3];
+        const story = result[4];
+        App.setQueryResult("[" + name + " / " + dec + " / " + mag + " / " + ent + " / " + story + "]");
+      } else {
+        alert(error)
+      }
+    })
+    // let result = await lookUptokenIdToStarInfo(starId).send({ from: this.account });
+    // App.setQueryResult("[" + name + " / " + dec + " / " + mag + " / " + ent + " / " + story + "]");
   }
 
 };
